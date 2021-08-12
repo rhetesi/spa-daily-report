b1232c51ad32561f33f718e6dadb20dc0fe11211
rhetesi <rhetesi@gmail.com>
2021-08-12 17:57:49 +0200

    Fix: wellnessdatas model

--- frontend/src/app/model/wellnessdatas.ts
+++ frontend/src/app/model/wellnessdatas.ts
@@ -3,6 +3,13 @@ import { User } from "./user";
 export class Wellnessdatas {
   _id: string = '';
   date: Date = new Date();
-  sumOfGuests: number = 0;
+  massage: number = 0;
+  pediAndManiCure: number = 0;
+  cosmetics: number = 0;
+  medicaExamination: number = 0;
+  mudSale: number = 0;
+  mudTreatment: number = 0;
+  weightBath: number = 0;
+  phisioTherapy: number = 0;
   dataLogger: User = new User();
 }
