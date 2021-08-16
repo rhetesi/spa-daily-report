2f6b135139d2dadd4b6d3e96f205bbe044cf173d
rhetesi <rhetesi@gmail.com>
2021-08-16 21:38:17 +0200

    User edit component works

--- frontend/src/app/page/user-edit/user-edit.component.ts
+++ frontend/src/app/page/user-edit/user-edit.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { FieldBase } from 'src/app/areus-form/model/field-base';
+import { InputField } from 'src/app/areus-form/model/input-field';
+import { SelectField } from 'src/app/areus-form/model/select-field';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/service/user.service';
 
@@ -15,6 +19,9 @@ export class UserEditComponent implements OnInit {
   user$: Observable<User> = this.ar.params.pipe(
     switchMap(params => this.userService.get(params.id))
   );
+  user: User = new User();
+  fields: FieldBase<any>[] = [];
+  showForm: boolean = false;
 
   constructor(
     private userService: UserService,
@@ -23,6 +30,38 @@ export class UserEditComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.user$.subscribe(
+      user => {
+        this.user = user;
+        this.setForm();
+        this.showForm = true;
+      }
+    )
+  }
+
+  setForm(): void {
+    this.fields = [
+      new InputField({ key: '_id', label: '', type: 'hidden', value: this.user._id }),
+      new InputField({ key: 'lastName', label: 'Vezetéknév', type: 'text', value: this.user.lastName as string,  validators: [Validators.required, Validators.pattern(/^[A-Ü][a-ü]{0,19}/)], errorMessage: 'Adjon meg egy 3-20 karakter közötti vezetéknevet!' }),
+      new InputField({ key: 'firstName', label: 'Keresztnév', type: 'text', value: this.user.firstName as string }),
+      new InputField({ key: 'email', label: 'Email', type: 'text', value: this.user.email as string }),
+      new InputField({ key: 'address', label: 'Cím', type: 'text', value: this.user.address as string }),
+      new InputField({ key: 'role', label: 'Jogosultság', type: 'number', value: this.user.role as unknown as number }),
+      //new SelectField({key: 'active', label: 'Aktív?', type: 'boolean', value: this.user.active}),
+      new InputField({key: 'password', label: 'Jelszó', type: 'text', value: this.user.password as string}),
+    ]
+
+    /**
+     * _id: string = '';
+  firstName?: string = '';
+  lastName?: string = '';
+  email?: string = '';
+  address?: string = '';
+  role?: number = 0;
+  active?: boolean = true;
+  password?: string = '';
+  accessToken?: string = '';
+     */
   }
 
   onSave(user: User): void {
